4a4f410e53f9a2c751b618e95fd2be1e0ae932ea
Ellis Madagan <em1382@messiah.edu>
2016-03-01 22:16:15 -0500

    Added quine.py.
    Creates a "Hello World" program, then imports and calls it in one go.

---
+++ quine.py
@@ -0,0 +1,14 @@
+# This program creates a "Hello World!" program, then runs it.
+
+def main():
+    program = ["def sayHello():", "\n\tprint('Hello World!')"]
+    f = open('hello.py', 'w+')
+    for line in program:
+        f.write(line)
+    f.close()
+
+    import hello
+    hello.sayHello()
+
+main()
+    
